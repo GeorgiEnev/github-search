cbf7295ee4051f4554d7235e820191cabde24438
GeorgiEnev <gogoenev@gmail.com>
2026-02-04 19:31:23 +0200

    add basic functionality for search and display of  a user

---
+++ script.js
@@ -0,0 +1,41 @@
+const input = document.getElementById("input");
+const searchBtn = document.getElementById("searchBtn");
+const details = document.getElementById("details");
+
+searchBtn.addEventListener("click", onSearch);
+
+async function onSearch() {
+    const username = input.value.trim();
+    const URL = `https://api.github.com/users/${username}`;
+
+    if (!username) {
+        details.innerHTML = "<p>Please enter a username.</p>";
+        return;
+    }
+
+    try {
+        const resp = await fetch(URL);
+
+        if (!resp.ok) {
+            details.innerHTML = "<p>User not found.</p>";
+            return;
+        }
+
+        const user = await resp.json();
+
+        displayUser(user);
+    } catch {
+        details.innerHTML = "<p>Something went wrong.</p>";
+    }
+}
+
+function displayUser(user) {
+    details.innerHTML = `
+    <div class="user-info">
+      <h2>${user.name}</h2>
+      <p class="username">@${user.login}</p>
+      <p>Public repositories: ${user.public_repos}</p>
+    </div>
+  `;
+    
+}
